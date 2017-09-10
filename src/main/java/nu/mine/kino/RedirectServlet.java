15e56ea1c9716b2489abf3b77d116dac2933127c
Masatomi KINO <masatomix@ki-no.org>
2017-09-11 00:56:59 +0900

    Qiitaの記事向けにUpdate

--- src/main/java/nu/mine/kino/RedirectServlet.java
+++ src/main/java/nu/mine/kino/RedirectServlet.java
@@ -1,12 +1,13 @@
 package nu.mine.kino;
 
-import static nu.mine.kino.Constants.*;
 import static nu.mine.kino.utils.JSONUtils.*;
-import static nu.mine.kino.utils.Utils.*;
+import static org.apache.commons.codec.binary.Base64.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -16,70 +17,46 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.StatusType;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 
 import lombok.extern.slf4j.Slf4j;
-import nu.mine.kino.utils.Utils;;
 
-/**
- * Servlet implementation class RedirectServlet
- */
-// @WebServlet("/RedirectServlet")
 @Slf4j
 public class RedirectServlet extends HttpServlet {
-    private static final long serialVersionUID = -4054957515370180691L;
-
-    private ResourceBundle bundle = null;
-
     private static final String PARAM_AUTHORIZATION_CODE = "code";
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        String propertyFile = getServletConfig().getInitParameter("property");
-        try {
-            bundle = ResourceBundle.getBundle(propertyFile);
-            doSettings(bundle);
-        } catch (java.util.MissingResourceException e) {
-            String message = "設定ファイルが存在しません。クラスパス上に {}.propertiesを配置してください。({})";
-            log.error(message, propertyFile, e.getMessage());
-            throw new ServletException(e);
-        }
-    }
-
-    private void doSettings(ResourceBundle bundle) {
-        Enumeration<String> keys = bundle.getKeys();
-        while (keys.hasMoreElements()) {
-            String key = keys.nextElement();
-            log.debug("key[{}]:{}", key, bundle.getString(key));
-        }
-    }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
 
-        String redirect_url = getRedirect_url(request);
+        String redirect_url = new String(request.getRequestURL());
         String encodedRedirectUrl = URLEncoder.encode(redirect_url, "UTF-8");
         log.debug("redirect_url:[{}]", redirect_url);// ココでセットするURLは、通常OAuthサーバ側に登録されているURLでなければならない。
 
         String client_id = bundle.getString("client_id");
         String client_secret = bundle.getString("client_secret");
-
         String authorization_endpoint = bundle
                 .getString("authorization_endpoint");
         String token_endpoint = bundle.getString("token_endpoint");
-
         String userinfo_endpoint = bundle.getString("userinfo_endpoint");
         String jwks_uri = bundle.getString("jwks_uri");
 
@@ -96,9 +73,6 @@ public class RedirectServlet extends HttpServlet {
                     + "state=%3$s&" //
                     + "nonce=%4$s&" //
                     + "response_type=%5$s&"//
-                    // + "prompt=login+consent&"//
-                    // + "prompt=select_account&"//
-                    // + "display=popup&"//
                     + "scope=%6$s";
 
             // CSRF対策のための、stateを設定
@@ -106,7 +80,7 @@ public class RedirectServlet extends HttpServlet {
 
             String nonce = getRandomString();
             String response_type = "code";
-            String scope = getScope();
+            String scope = "openid+profile+email";
 
             String oauth_server_url = String.format(oauth_server_url_format,
                     client_id, //
@@ -117,31 +91,24 @@ public class RedirectServlet extends HttpServlet {
                     scope);
             log.debug(oauth_server_url);
 
-            HttpSession session = request.getSession(true);
-            log.debug("はじめのsession id: {}", session.getId());
-            log.debug("session state: {}", state);
-            session.setAttribute(SESSION_STATE, state);
-            session.setAttribute(SESSION_NONCE, nonce);
+            // HttpSession session = request.getSession(true);
+            // session.setAttribute(SESSION_STATE, state);
+            // session.setAttribute(SESSION_NONCE, nonce);
 
             // OAuth Serverへリダイレクト
             response.sendRedirect(oauth_server_url);
 
         } else {
             // Access TokenとID Tokenの取得処理
-
             // CSRF対策のための、パラメタから取得したヤツと、Sessionにあるヤツの値を確認
-            checkCSRF(request);
+            // checkCSRF(request);
 
-            String proxy = "http://client.example.com:8888";
             String result = getAccessTokenJSON(token_endpoint, redirect_url,
-                    client_id, client_secret, authorizationCode,
-                    Utils.createSecureClient(proxy));
+                    client_id, client_secret, authorizationCode);
 
             try {
                 Map<String, Object> map = json2Map(result);
-                String key = getAccess_token_key();
-
-                String accessToken = (String) map.get(key);
+                String accessToken = (String) map.get("access_token");
                 String id_token = (String) map.get("id_token");
 
                 log.debug("access_token: {}", accessToken);
@@ -154,8 +121,7 @@ public class RedirectServlet extends HttpServlet {
                 out.append(toPrettyStr(map));
                 out.append("\n\n");
 
-                // OpenID Connect対応でないと、id_tokenが返ってこない場合もある。
-                if (StringUtils.isNotEmpty(id_token)) {
+                {
                     printIdToken(id_token, out);
                     boolean checkResult = checkIdToken(id_token, jwks_uri,
                             client_secret);
@@ -163,36 +129,28 @@ public class RedirectServlet extends HttpServlet {
                 }
                 out.append("\n\n");
 
-                // 基本このチェック不要だけど、一応入れてる。(OpenID Connectでない場合はもしかしたら独自仕様の場合アリ)
-                if (StringUtils.isNotEmpty(userinfo_endpoint)) {
+                {
                     log.debug("Userinfo Endpoint Server:{}", userinfo_endpoint);
                     String userInfoJSON = getResource(userinfo_endpoint,
-                            accessToken, Utils.createSecureClient(proxy));
+                            accessToken);
                     out.append("User Information: \n");
                     out.append(toPrettyStr(json2Map(userInfoJSON)));
                 }
-
-                // String introspection_endpoint =
-                // "http://oauth.example.com:8880/api/introspection";
-                // String introspection =
-                // getIntrospection(introspection_endpoint,
-                // accessToken, Utils.createSecureClient(proxy));
-                // log.debug("introspection: {}", introspection);
-
-                String sample_endpoint = getSampleEndpoint();
-                if (StringUtils.isNotEmpty(sample_endpoint)) {
+                {
+                    String sample_endpoint = bundle
+                            .getString("sample_endpoint");
                     log.debug("Sample Endpoint: {}", sample_endpoint);
                     String sampleData = getResource(sample_endpoint,
-                            accessToken, Utils.createSecureClient(proxy));
+                            accessToken);
                     log.debug("Sample Data: {}", sampleData);
                 }
-
             } catch (BadRequestException e) {
                 throw new ServletException(e);
             }
         }
     }
 
+<<<<<<< HEAD
     // private static String getIntrospection(String introspection_endpoint,
     // String accessToken, Client client) {
     // MultivaluedMap<String, String> formParams = new
@@ -258,13 +216,15 @@ public class RedirectServlet extends HttpServlet {
         return result;
     }
 
+=======
+>>>>>>> feature/forQiita_spike
     private void printIdToken(String id_token, PrintWriter out)
             throws JsonProcessingException, IOException {
 
         String[] id_token_parts = id_token.split("\\.");
 
-        String ID_TOKEN_HEADER = decodeBase64String(id_token_parts[0]);
-        String ID_TOKEN_PAYLOAD = decodeBase64String(id_token_parts[1]);
+        String ID_TOKEN_HEADER = new String(decodeBase64(id_token_parts[0]));
+        String ID_TOKEN_PAYLOAD = new String(decodeBase64(id_token_parts[1]));
         // String ID_TOKEN_SIGNATURE =
         // base64DecodeStr(id_token_parts[2]);
         log.debug("ID_TOKEN_HEADER: {}", ID_TOKEN_HEADER);
@@ -292,5 +252,233 @@ public class RedirectServlet extends HttpServlet {
         // TODO Auto-generated method stub
         doGet(request, response);
     }
+
+    private ResourceBundle bundle = null;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        String propertyFile = getServletConfig().getInitParameter("property");
+        try {
+            bundle = ResourceBundle.getBundle(propertyFile);
+            doSettings(bundle);
+        } catch (java.util.MissingResourceException e) {
+            String message = "設定ファイルが存在しません。クラスパス上に {}.propertiesを配置してください。({})";
+            log.error(message, propertyFile, e.getMessage());
+            throw new ServletException(e);
+        }
+    }
+
+    private void doSettings(ResourceBundle bundle) {
+        Enumeration<String> keys = bundle.getKeys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            log.debug("key[{}]:{}", key, bundle.getString(key));
+        }
+    }
+
+    private static String getRandomString() {
+        return RandomStringUtils.randomAlphanumeric(40);
+    }
+
+    /**
+     * レスポンスのHTTP Response Statusのチェック。400番台、500番台の場合例外
+     * 
+     * @param restResponse
+     * @throws ServletException
+     */
+    private static void checkAccessTokenResult(Response restResponse)
+            throws ServletException {
+        StatusType statusInfo = restResponse.getStatusInfo();
+        switch (statusInfo.getFamily()) {
+        case CLIENT_ERROR:
+        case SERVER_ERROR:
+            String message = String.format("Status: %s:[%s]",
+                    statusInfo.getStatusCode(), statusInfo.getReasonPhrase());
+            log.error("{}", restResponse.getStatusInfo());
+            throw new ServletException(message);
+        default:
+            break;
+        }
+    }
+
+    /**
+     * AccessToken取得のためのMapを作成する。Qiitaだけ、
+     * 
+     * <pre>
+     * Content-Type: application/x-www-form-urlencoded
+     * </pre>
+     * 
+     * を受け付けないので、パラメタ生成の処理を分けている。
+     * 
+     * @param redirect_url
+     * @param client_id
+     * @param client_secret
+     * @param authorizationCode
+     * @param client
+     * @param mediaType
+     * @return
+     */
+    private static Map<String, ?> createMap(String redirect_url,
+            String client_id, String client_secret, String authorizationCode,
+            Client client) {
+
+        String grant_type = "authorization_code";
+        MultivaluedMap<String, String> formParams = new MultivaluedHashMap<String, String>();
+        formParams.putSingle("redirect_uri", redirect_url);
+        formParams.putSingle("grant_type", grant_type);
+        formParams.putSingle("client_id", client_id);
+        formParams.putSingle("client_secret", client_secret);
+        formParams.putSingle("code", authorizationCode);
+
+        return formParams;
+
+    }
+
+    private static String getAccessTokenJSON(String oauth_server,
+            String redirect_url, String client_id, String client_secret,
+            String authorizationCode) throws ServletException {
+        String result = null;
+
+        Client client = ClientBuilder.newClient();
+        Map<String, String> formParams = (Map<String, String>) createMap(
+                redirect_url, client_id, client_secret, authorizationCode,
+                client);
+
+        log.debug("OAuthServer:{}", oauth_server);
+        Response restResponse = client //
+                .target(oauth_server) //
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .post(Entity.entity(formParams,
+                        MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        result = restResponse.readEntity(String.class);
+        log.debug("result: {}", result);
+        checkAccessTokenResult(restResponse);
+
+        return result;
+
+    }
+
+    private static String getResource(String target, String accessToken) {
+        Client client = ClientBuilder.newClient();
+        Response restResponse = client.target(target)
+                .queryParam("schema", "openid")//
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + accessToken).get();
+
+        String result = restResponse.readEntity(String.class);
+        log.debug(result);
+        return result;
+    }
+
+    private static boolean checkHSSignature(SignedJWT decodeObject,
+            byte[] sharedSecret) throws JOSEException {
+        JWSVerifier verifier = new MACVerifier(sharedSecret);
+        boolean verify = decodeObject.verify(verifier);
+        log.debug("valid？: {}", verify);
+        return verify;
+    }
+
+    private static boolean checkRSSignature(SignedJWT decodeObject,
+            String jwks_uri) throws JOSEException, IOException, ParseException {
+        // Headerから KeyIDを取得して、
+        String keyID = decodeObject.getHeader().getKeyID();
+        log.debug("KeyID: {}", keyID);
+
+        // ちなみにGoogleは
+        // http://qiita.com/trysmr/items/e8d4225ff6a603e9e21a によると
+        // https://www.googleapis.com/oauth2/v3/certs
+        // ちなみにAuthleteは
+        // https://[サーバ名]/api/jwks
+        // だがデフォルトだとかえんないっぽい。設定しないとかな。
+        RSAKey rsaKey = getRSAKey(jwks_uri, keyID);
+        JWSVerifier verifier = new RSASSAVerifier(rsaKey);
+        boolean verify = decodeObject.verify(verifier);
+        log.debug("valid？: {}", verify);
+        return verify;
+    }
+
+    private static boolean checkIdToken(String id_token, String jwks_uri,
+            String secret) throws ServletException {
+        // ココ手動でクチャクチャやってるけど、Nimbusを使って書き換え。
+        // String[] id_token_parts = id_token.split("\\.");
+        //
+        // String ID_TOKEN_HEADER = base64DecodeStr(id_token_parts[0]);
+        // String ID_TOKEN_PAYLOAD = base64DecodeStr(id_token_parts[1]);
+        // // String ID_TOKEN_SIGNATURE =
+        // // base64DecodeStr(id_token_parts[2]);
+        // log.debug("ID_TOKEN_HEADER: {}", ID_TOKEN_HEADER);
+        // log.debug("ID_TOKEN_PAYLOAD: {}", ID_TOKEN_PAYLOAD);
+        // // log.debug("ID_TOKEN_SIGNATURE: {}", ID_TOKEN_SIGNATURE);
+
+        try {
+            // JWTの仕様に基づいて、デコードしてみる。
+            SignedJWT decodeObject = SignedJWT.parse(id_token);
+            log.debug("Header : " + decodeObject.getHeader());
+            log.debug("Payload: " + decodeObject.getPayload());
+            log.debug("Sign   : " + decodeObject.getSignature());
+
+            JWSAlgorithm algorithm = decodeObject.getHeader().getAlgorithm();
+            JWTClaimsSet set = decodeObject.getJWTClaimsSet();
+            log.debug("Algorithm: {}", algorithm.getName());
+            log.debug("Subject: {}", set.getSubject());
+            log.debug("Issuer: {}", set.getIssuer());
+            log.debug("Audience: {}", set.getAudience());
+            log.debug("Nonce: {}", set.getClaim("nonce"));
+            log.debug("now before ExpirationTime?: {}",
+                    new Date().before(set.getExpirationTime()));
+
+            if (algorithm.getName().startsWith("HS")) {
+                log.debug("共通鍵({})", algorithm.getName());
+                byte[] sharedSecret = secret.getBytes(); // バイト列に変換
+                return checkHSSignature(decodeObject, sharedSecret);
+            } else {
+                log.debug("公開鍵({})", algorithm.getName());
+                return checkRSSignature(decodeObject, jwks_uri);
+            }
+
+        } catch (ParseException e) {
+            log.warn("サーバの公開鍵の取得に失敗しています.{}", e.getMessage());
+        } catch (IOException e) {
+            log.warn("サーバの公開鍵の取得に失敗しています.{}", e.getMessage());
+        } catch (JOSEException e) {
+            log.warn("Verify処理に失敗しています。{}", e.getMessage());
+        }
+        return false;
+
+    }
+
+    private static final long serialVersionUID = -4054957515370180691L;
+
+    // private static final String SESSION_STATE = "state";
+    //
+    // private static final String PARAM_STATE = "state";
+
+    // private static final String SESSION_NONCE = "nonce";
+    //
+    // private static final String PARAM_NONCE = "nonce";
+
+    // /**
+    // * CSRF対策。 セッションが存在するか、存在するなら、セッション内のstate 属性とリクエストパラメタのstateパラメタの値の一致チェック
+    // *
+    // * @param request
+    // * @throws ServletException
+    // */
+    // private static void checkCSRF(HttpServletRequest request)
+    // throws ServletException {
+    // HttpSession session = request.getSession(false);
+    // if (session == null) {
+    // throw new ServletException("Bad Request(session is null.)");
+    // }
+    //
+    // log.debug("Redirect後のsession id: {}", session.getId());
+    // String requestState = request.getParameter(PARAM_STATE);
+    // String sessionState = (String) session.getAttribute(SESSION_STATE);
+    // log.debug("requestState:[{}]", requestState);
+    // log.debug("sessionState:[{}]", sessionState);
+    // if (!requestState.equals(sessionState)) {
+    // throw new ServletException("前回のリクエストと今回のstate値が一致しないため、エラー。");
+    // }
+    // }
 
 }
---
+++ src/main/java/nu/mine/kino/utils/JSONUtils.java
@@ -0,0 +1,83 @@
+/******************************************************************************
+ * Copyright (c) 2010 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
+ * $Id$
+ ******************************************************************************/
+//作成日: 2017/07/23
+
+package nu.mine.kino.utils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+
+/**
+ * @author Masatomi KINO
+ * @version $Revision$
+ */
+public class JSONUtils {
+
+    public static Map<String, Object> json2Map(String result)
+            throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(result,
+                new TypeReference<Map<String, Object>>() {
+                });
+    }
+
+    public static String toPrettyStr(Object obj)
+            throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper()
+                .enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper.writeValueAsString(obj);
+    }
+
+    /**
+     * 指定されたURLから、JWKを取得
+     * 
+     * @param url
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
+    private static JWKSet getJWKSet(String url)
+            throws IOException, ParseException {
+        // HTTP connect timeout in milliseconds
+        int connectTimeout = 1000;
+
+        // HTTP read timeout in milliseconds
+        int readTimeout = 1000;
+
+        // JWK set size limit, in bytes
+        int sizeLimit = 10000;
+
+        // The URL
+        JWKSet publicKeys = JWKSet.load(new URL(url), connectTimeout,
+                readTimeout, sizeLimit);
+        return publicKeys;
+
+    }
+
+    public static RSAKey getRSAKey(String url, String keyID)
+            throws IOException, ParseException {
+        JWKSet publicKeys = getJWKSet(url);
+        JWK key = publicKeys.getKeyByKeyId(keyID);
+        RSAKey rsaKey = RSAKey.parse(key.toJSONObject());
+        return rsaKey;
+    }
+
+}
